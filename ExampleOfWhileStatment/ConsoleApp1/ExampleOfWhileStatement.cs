4130873340b75bd8a3c3939f5553db2ceb8acd42
Kindra Bilodeau <you@example.com>
2019-03-16 20:36:31 -0400

    Example of a while statement

---
+++ ExampleOfWhileStatment/ConsoleApp1/ExampleOfWhileStatement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            int total = multiply(a, b);
+            Console.ReadLine();
+        }
+
+        private static int multiply(int a, int b)
+        {
+            int c = 0;
+            while(c < b)
+            {
+                int valuea = a + a;
+                c++;
+            }
+            return a; 
+        }
+    }
+}
