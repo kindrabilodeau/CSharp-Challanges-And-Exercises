9424cadccf456f587586d1745140c80ee55de172
Kindra Bilodeau <you@example.com>
2019-03-16 20:34:39 -0400

    Added exercise that adds up the digits in a number

---
+++ AddTheNumbers/AddTheNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTheNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 123;
+            
+            int acc = 0; 
+            while (n > 0 )
+            {
+                acc += n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine($"{acc}"); 
+        }
+    }
+}
