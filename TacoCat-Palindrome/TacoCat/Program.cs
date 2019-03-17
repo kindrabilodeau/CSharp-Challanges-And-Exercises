38e69f0e79d01342de09c38950fe786fef54f4eb
Kindra Bilodeau <you@example.com>
2019-03-16 20:47:14 -0400

    Exercise:Find if a string is a palindrome

---
+++ TacoCat-Palindrome/TacoCat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacoCat
+{
+    class Program
+    {  
+        //Test to see if string input is a palindrome.
+
+        static void Main(string[] args)
+        {
+            string test1 = "tacocat";
+            string test2 = "racecar";
+            string test3 = "bingo";
+            string test4 = "taco";
+            string test5 = "box";
+            string test6 = "evil olive";
+
+            Pal(test1);
+            Pal(test2);
+            Pal(test3);
+            Pal(test4);
+            Pal(test5);
+            Pal(test6);
+        }
+
+        private static void Pal(string test)
+        {
+            bool check = false;
+
+            for (int i = 0, j = test.Length -1 ; i < test.Length; i++, j--)
+            {
+                if (test[i] == test[j])
+                    check = true;
+                else
+                    check = false; 
+            }
+
+            Console.WriteLine($"{test} = {check}");
+            Console.ReadLine();
+        }
+    }
+}
