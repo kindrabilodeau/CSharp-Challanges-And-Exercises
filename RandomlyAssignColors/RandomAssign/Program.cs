5fdb9b1011899ba54b1cdcaa5ee044cf972f7a9e
Kindra Bilodeau <you@example.com>
2019-03-16 20:41:27 -0400

    Exercise: Randomly assigns colors

---
+++ RandomlyAssignColors/RandomAssign/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] x = new char[32];
+            int purple = 16;
+            int green = 16;
+            Random r = new Random();
+            Console.WriteLine($"Green:{green}, Purple:{purple}");
+            for (int i = 0; i < x.Length; i++)
+            {
+                int m = r.Next() % 2;
+                Console.Write($"{m}, ");
+                if (m == 0 && purple > 0 || green == 0)
+                {
+                    x[i] = 'P';
+                    purple--;
+                }
+                else if (m == 1 && green > 0 || purple == 0)
+                {
+                    x[i] = 'G';
+                    green--;
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < x.Length; i++)
+                Console.Write($"{x[i]}, ");
+            Console.WriteLine();
+            Console.WriteLine($"Green:{green}, Purple:{purple}");
+        }
+    }
+}
