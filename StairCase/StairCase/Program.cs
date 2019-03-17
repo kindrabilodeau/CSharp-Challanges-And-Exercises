9a89364bf06603e5068ac2cac850c801e647de3f
Kindra Bilodeau <you@example.com>
2019-03-16 20:44:34 -0400

    Exercise:Make a staircase out of #

---
+++ StairCase/StairCase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            MakeStairCase(n);
+            Console.ReadLine();
+        }
+
+        private static void MakeStairCase(int n)
+        {
+            int t = n;
+            int g = 0;
+
+            while (n > 0)
+            {
+                int x = 0; 
+                t = n;    
+                g++;   
+                while (t > 0)   
+                {
+                    t--;   
+                    Console.Write(' ');
+                }
+                while (x < g)
+                {
+                    Console.Write("#");
+                    x++;
+                }
+                Console.WriteLine();
+                n--;
+            }
+        }
+    }
+}
